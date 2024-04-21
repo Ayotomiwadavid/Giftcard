e7791827240fa9b206626ee7f15ef438a5cd5d71
Techthoth <ayotomiwadavid@proton.me>
2024-04-21 14:42:46 -0700

    saving progress

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,7 +8,7 @@ const AppNavbar = () => {
                 <span className="self-center text-2xl font-bold capitalize text-white whitespace-nowrap dark:text-white">checkr</span>
             </Navbar.Brand>
             <div className="flex md:order-2 items-center justify-center space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <Button  class="text-[#1363DF] bg-white hover:bg-[#f5f5f5] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:text-white dark:hover:bg-blue-700 dark:focus:ring-blue-800">Verify Card</Button>
+                <Button  class="text-[#1363DF] bg-white hover:bg-[#f5f5f5] rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:text-white dark:hover:bg-blue-700 dark:focus:ring-blue-800">Verify Card</Button>
                 <Navbar.Toggle className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[#1363DF] bg-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'/>
             </div>
             <Navbar.Collapse className='bg-[#1363DF] mt-2'>
