3bfa32cf37c0e4ca49839fea3297e040ee6d1d9b
Techthoth <ayotomiwadavid@proton.me>
2024-04-21 15:42:03 -0700

    saving progress

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
-import React, { useState } from 'react'
-// import Header from './component/Header'
-import Herosection from './component/Herosection'
-import About from './component/About'
-import Contactsection from './component/Contactsection'
-import Productsection from './component/Productsection'
-import Rating from './Newsletter'
-import Footer from './component/Footer'
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import AppNavbar from './component/Navbar'
+import React, { useState, useEffect } from 'react';
+import Herosection from './component/Herosection';
+import About from './component/About';
+import Contactsection from './component/Contactsection';
+import Productsection from './component/Productsection';
+import Rating from './Newsletter';
+import Footer from './component/Footer';
+import AppNavbar from './component/Navbar';
+import ScrollToTop from 'react-scroll-to-top';
 
 const App = () => {
-  let [controllerVisibility, setControllerVisibility] = useState(false)
-  const handleClick = () => {
-    document.documentElement.scrollTop = 0;
-  }
-  let handleControllerVisibility = () => {
-    let pos = document.documentElement.scrollTop;
-    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100) / calcHeight)
-    if (pos > 100) {
-      setControllerVisibility(true)
-    } else {
-      setControllerVisibility(false)
-    }
-  }
-  window.onscroll = handleControllerVisibility;
-  window.onload = handleControllerVisibility;
   return (
-    <main className='bg-[#fff] scro' id='top'>
-      <aside className={controllerVisibility ? 'fixed flex items-center justify-center h-[50px] w-[50px] rounded-full bg-[#1363DF] bottom-[20px] right-[10px] shadow-md z-10 p-1' : 'fixed hidden items-center justify-center h-[50px] w-[50px] rounded-full bg-[#1363DF] bottom-[20px] right-[10px] shadow-md z-10 p-1'} onClick={handleClick}>
-        <div className='flex items-center justify-center h-full w-full rounded-full bg-white bottom-[20px] right-[10px] shadow-md z-10'>
-          <ArrowUpwardIcon style={{ fontSize: '30', color: '#1363DF' }} />
-        </div>
-      </aside>
+    <main className='bg-[#fff]' id='top'>
+      <div className='fixed bottom-[20px] right-[10px] w-[50px] h-[50px] rounded-full'>
+        <ScrollToTop smooth 
+        viewBox="0 0 32 32"
+        svgPath = 'm26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z'
+        color='white'
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#1363DF',
+          height: '50px',
+          width: '50px',
+          borderRadius: '50%',
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+        }} />
+      </div>
       <AppNavbar />
       <Herosection />
       <About />
@@ -41,7 +35,7 @@ const App = () => {
       <Rating />
       <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
