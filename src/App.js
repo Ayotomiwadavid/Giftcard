175012e5a8fdb5356e29cd29ab9e66a07c3ce303
Techthoth <ayotomiwadavid@proton.me>
2024-04-27 06:02:13 -0700

    saving progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Home from './Pages/Home';
+import { Route, Router } from 'react-router-dom';
+import Productdetails from './Pages/Productdetails';
 
 
 const App = () => {
   return (
-    <Home />
+    <Router>
+       <Route path='/' element={<Home />}/>
+       <Route path='/product/:id' element={<Productdetails />} />
+       <Route path='*' element='Page not Found' />
+    </Router>
   );
 }
 
