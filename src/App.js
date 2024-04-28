2acb3920ef5bd72a730bb948d46ed8b977050d67
Techthoth <ayotomiwadavid@proton.me>
2024-04-28 02:32:13 -0700

    saving progress

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Home from './Pages/Home';
-import { Route, Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Productdetails from './Pages/Productdetails';
 
 
 const App = () => {
   return (
-    <Router>
+    <Routes>
        <Route path='/' element={<Home />}/>
        <Route path='/product/:id' element={<Productdetails />} />
        <Route path='*' element='Page not Found' />
-    </Router>
+    </Routes>
   );
 }
 
--- src/Pages/Productdetails.js
+++ src/Pages/Productdetails.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import AppNavbar from '../component/Navbar'
+import Productdetailscard from '../component/Productdetailscard'
+import Ordersummary from '../component/Ordersummary'
 
 const Productdetails = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <main>
+            <AppNavbar />
+            <section className='flex w-full px-6 py-10 items-center justify-center'>
+                <Productdetailscard />
+                <Ordersummary />
+            </section>
+        </main>
+    )
 }
+
 
 export default Productdetails
